24653320175615f574647cc3f6d59eddb6d3bde6
km <km>
2026-07-01 10:53:41 +0700

    [FIX]revision manifest

--- customize/project_code/__manifest__.py
+++ customize/project_code/__manifest__.py
@@ -1,25 +1,41 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "project_code",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'name': "Project Code",
+    'summary': "Solusi efisien untuk manajemen alur kerja dan pelacakan progres proyek secara real-time.",
 
     'description': """
-Long description of module's purpose
+        Modul Project Code membantu organisasi dalam mengelola siklus hidup proyek 
+        secara komprehensif. Fitur utama mencakup:
+        
+        *   Manajemen tugas yang intuitif dan terintegrasi.
+        *   Pelaporan progres otomatis untuk setiap fase proyek.
+        *   Kolaborasi tim yang ditingkatkan melalui dasbor terpusat.
+        *   Analisis data untuk pengambilan keputusan yang lebih cepat.
+        
+        Modul ini dirancang khusus untuk Odoo 18 dengan memanfaatkan performa 
+        framework terbaru untuk memberikan pengalaman pengguna yang responsif.
     """,
 
-    'author': "My Company",
+    'author': "Kristian Maulana",
     'website': "https://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Operations/Project',
     'version': '0.1',
     "license": "AGPL-3",
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'crm', 'project', 'account'],
+    'depends': [
+        'base',
+        'project',
+        'account',
+        'project_teams',
+        'project_stock',
+        'project_stock_account',
+        'stock'
+    ],
     # always loaded
     'data': [
         "data/res_partner_lob.xml",
@@ -37,4 +53,7 @@ Long description of module's purpose
     'demo': [
         'demo/demo.xml',
     ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
 }
